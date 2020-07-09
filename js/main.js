fcc4e0d26a63cdaeee9bde738fa466a7c940540e
Kostiuk Pavlo <kostiukpav@gmail.com>
2020-07-09 12:37:13 +0300

    object

--- js/main.js
+++ js/main.js
@@ -1,40 +1,12 @@
-var count = 0;
-
-function cc(card) {
-    // Only change code below this line
-
-    switch (card) {
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-            count++;
-            break;
-        case 10:
-        case "J":
-        case "Q":
-        case "K":
-        case "A":
-            count--;
-            break;
-    }
-    if (count > 0) return count + " Bet";
-    return count + " Hold";
+var myDog = {
+    name: "Lev",
+    legs: 4,
+    tails: 1,
+    friends: ["Sam", "Kat", "Mes"]
 
 
-    // Only change code above this line
-}
+};
 
-console.log(cc());
-console.log(cc(2));
-console.log(cc(2));
-console.log(cc(3));
-console.log(cc(8));
-console.log(cc());
-console.log(cc(9));
-console.log(cc());
-console.log(cc(10));
-console.log(cc("A"));
-console.log(cc());
-console.log(cc());
+console.log(myDog.friends[2]);
+console.log(myDog.legs);
+console.log(myDog.name);
