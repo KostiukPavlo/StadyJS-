f9eb1acf408eaed0fa1068e422c2c218ce2caf90
Kostiuk Pavlo <kostiukpav@gmail.com>
2020-07-09 16:08:32 +0300

    Object

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-var myDog = {
-    name: "Lev",
-    legs: 4,
-    tails: 1,
-    friends: ["Sam", "Kat", "Mes"]
-
-
+// Setup
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
 };
 
-console.log(myDog.friends[2]);
-console.log(myDog.legs);
-console.log(myDog.name);
+// Only change code below this line
+
+var playerNumberFirst = 12; // Change this line
+var playerNumberSecond = 15; // Change this line
+var playerNumberLast = 19; // Change this line
+var player1 = testObj[playerNumberFirst]; // Change this line
+var player2 = testObj[playerNumberSecond]; // Change this line
+var player3 = testObj[playerNumberLast]; // Change this line
+
+console.log(player1);
+console.log(player2);
+console.log(player3);
