d44da85ce1235a40ca46b3cf67277c7fd8abdc9e
Kostiuk Pavlo <kostiukpav@gmail.com>
2020-07-13 14:10:41 +0300

    object property

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 function checkObj(obj, checkProp) {
     // Only change code below this line
 
-
+    if (obj.hasOwnProperty(checkProp)) {
+        return console.log(obj[checkProp]);
+    } else {
+        return console.log("Not Found");
+    }
 
 
     return "Change Me!";
     // Only change code above this line
 }
+
+
+checkObj({
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
+}, "gift");
+checkObj({
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
+}, "gif");
+checkObj({
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
+}, "pet")
