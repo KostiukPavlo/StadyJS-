89d057395cbcd77b21c15372be36c7baec5709c2
samyrai2000 <samyrai2000@gmail.com>
2020-08-07 13:37:50 +0300

    07.08.2020

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
 // Setup
-var myStorage = {
-    "car": {
-        "inside": {
-            "glove box": "maps",
-            "passenger seat": "crumbs"
-        },
-        "outside": {
-            "trunk": "jack"
-        }
+var collection = {
+    2548: {
+        album: "Slippery When Wet",
+        artist: "Bon Jovi",
+        tracks: [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    2468: {
+        album: "1999",
+        artist: "Prince",
+        tracks: [
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    1245: {
+        artist: "Robert Palmer",
+        tracks: []
+    },
+    5439: {
+        album: "ABBA Gold"
     }
 };
 
-var gloveBoxContents = myStorage.car.inside['glove box']; // Change this line
+// Only change code below this line
+function updateRecords(id, prop, value) {
 
-console.log(gloveBoxContents);
+
+    return collection;
+}
+
+updateRecords(5439, "artist", "ABBA");
